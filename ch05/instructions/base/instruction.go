4523308257bbe2b5c53efa38de5bb13bf961abcc
Jeremy Zhang <14549512@qq.com>
2019-04-29 15:47:12 +0800

    add insts

---
+++ ch05/instructions/base/instruction.go
@@ -0,0 +1,46 @@
+package base
+
+import (
+	"jvmgo/ch05/rtda"
+)
+
+type Instruction interface {
+	FetchOperands(reader *BytecodeReader)
+	Execute(frame *rtda.Frame)
+}
+
+type NoOperandsInstruction struct{}
+
+func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader)  {
+
+}
+
+//func (self *NoOperandsInstruction) Execute(frame *rtda.Frame)  {
+//
+//}
+
+type BranchInstruction struct {
+	Offset int
+}
+
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader)  {
+	self.Offset = int(reader.ReadUint16())
+}
+
+type Index8Instruction struct {
+	Index uint
+}
+
+func (self *Index8Instruction) FetchOperands(reader *BytecodeReader)  {
+	self.Index = uint(reader.ReadUint8())
+}
+
+type Index16Instruction struct {
+	Index uint
+}
+
+func (self *Index16Instruction) FetchOperands(reader *BytecodeReader)  {
+	self.Index = uint(reader.ReadUint16())
+}
+
+
---
+++ ch05/instructions/stack/swap.go
@@ -0,0 +1,16 @@
+package stack
+
+import "jvmgo/ch05/rtda"
+import "jvmgo/ch05/instructions/base"
+
+type SWAP struct {
+	base.NoOperandsInstruction
+}
+
+func (self *SWAP) Execute(frame *rtda.Frame)  {
+	stack := frame.OperandStack()
+	slot1 := stack.PopSlot()
+	slot2 := stack.PopSlot()
+	stack.PushSlot(slot1)
+	stack.PushSlot(slot2)
+}
